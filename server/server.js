41782c8cbb528adf1e7705f9e2eef08f3d2fcb1f
Alfredo Diaz <alfredo.diaz.dev@gmail.com>
2024-11-15 16:28:42 -0600

    third intent

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cors())
 
 // API routes
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/", (req, res) => res.send("Puerto 4000 activo"))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
