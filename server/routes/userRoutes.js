89d12943dd92df0e2f70ee33fdb904c857b6ab28
Alfredo Diaz <alfredo.diaz.dev@gmail.com>
2024-11-16 11:10:13 -0600

    seventh

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,9 +3,6 @@ import { clerkWebhooks } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/webhooks', (req, res) => {
-    console.log("Ruta /webhooks llamada");
-    clerkWebhooks(req, res);
-});
+userRouter.post('/webhooks', clerkWebhooks);
 
 export default userRouter;
