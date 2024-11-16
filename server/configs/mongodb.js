93d540c57223ff4161af6d684a90eb2630c526fb
Alfredo Diaz <alfredo.diaz.dev@gmail.com>
2024-11-15 21:22:35 -0600

    Fourth intent

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    try {
+        mongoose.connection.on("connected", () => {
+            console.log("MongoDB está conectado");
+        });
 
-    mongoose.connection.on("connected", () => {
-        console.log("MongoDB está conectado")
-    });
+        await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+        console.log("Conexión a MongoDB exitosa");
+    } catch (error) {
+        console.error("Error al conectar a MongoDB:", error);
+        process.exit(1); // Salir del proceso con un error
+    }
+};
 
-}
-
-export default connectDB
+export default connectDB;
